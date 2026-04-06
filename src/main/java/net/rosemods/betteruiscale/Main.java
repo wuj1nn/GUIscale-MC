6c3b2d9e1ed67d942b6761eb742fb1e52d3d896c
wujinn <ilagjohninno@gmail.com>
2026-04-06 17:57:34 +0800

    fixed options 2

--- src/main/java/net/rosemods/betteruiscale/Main.java
+++ src/main/java/net/rosemods/betteruiscale/Main.java
@@ -1,6 +1,9 @@
 package net.rosemods.betteruiscale;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import net.rosemods.betteruiscale.mixin.ScreenInvoker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +18,17 @@ public class Main implements ClientModInitializer {
                 Config.getInstance().getGuiScale() > 0
                         ? Config.getInstance().getGuiScale()
                         : "disabled (using vanilla)");
+
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (!(screen instanceof VideoOptionsScreen)) return;
+            int sliderWidth = 200;
+            int sliderHeight = 20;
+            int x = scaledWidth / 2 - sliderWidth / 2;
+            int y = scaledHeight - 52;
+            FractionalScaleSliderWidget slider = new FractionalScaleSliderWidget(
+                    x, y, sliderWidth, sliderHeight
+            );
+            ((ScreenInvoker)(Object)screen).invokeAddDrawableChild(slider);
+        });
     }
 }
